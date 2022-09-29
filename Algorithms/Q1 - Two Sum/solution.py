bda03e0889a4ebd089db7a931bbbf8a14db3f914
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-09-29 18:26:06 +1000

    method 2 for python3

--- Algorithms/Q1 - Two Sum/solution.py
+++ Algorithms/Q1 - Two Sum/solution.py
@@ -1,7 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # solution 1: 3000ms, 15.6MB
-        for index1 in range(0, len(nums)):
-            for index2 in range(index1+1, len(nums)):
-                if (nums[index1]+nums[index2] == target):
+        # solution 1 - hard search: 3000ms, 15.6MB
+        for index1 in range(len(nums)):
+            for index2 in range(index1 + 1, len(nums)):
+                if (nums[index1] + nums[index2] == target):
                     return [index1, index2]
+
+        # solution 2 - hash map by dict: 28ms, 16.1MB
+        indexMap = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in indexMap:
+                return [i, indexMap.get(diff)]
+            else:
+                indexMap[nums[i]] = i
