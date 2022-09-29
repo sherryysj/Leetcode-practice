44200856f1707fc2a4bcb6fef1f6edf9d0afa64a
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-09-29 17:18:09 +1000

    improve java code

--- Q1 - Two Sum/solution.java
+++ Q1 - Two Sum/solution.java
@@ -1,17 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // solution 1: 42ms, 41.3MB
-        int[] solution = new int[2];
-        outerloop:
+        // solution 1: 55ms, 41.4MB
         for (int index1=0; index1<nums.length; index1++){
             for (int index2=index1+1; index2<nums.length; index2++){
                 if(nums[index1]+nums[index2]==target){
-                    solution[0] = index1;
-                    solution[1] = index2;
-                    break outerloop;
+                    return new int[] {index1,index2};
                 };
             };
         };
-        return solution;
-    };
+        return new int[] {};
+    }
 }
