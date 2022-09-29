4d73d60954cb3687230f0cc629a3c32614f1ab7e
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-09-29 18:18:24 +1000

    clear code

--- Algorithms/Q1 - Two Sum/solution.java
+++ Algorithms/Q1 - Two Sum/solution.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         // solution 1 - hard search: 55ms, 41.4MB
-        /* 
         for (int index1 = 0; index1 < nums.length; index1++) {
             for (int index2 = index1 + 1; index2 < nums.length; index2++) {
                 if (nums[index1] + nums[index2] == target) {
@@ -10,12 +9,11 @@ class Solution {
             }
         }
         return new int[] {};
-        */
         
         // solution 2 - hash map: 2ms, 41.8MB
         HashMap<Integer, Integer> indexMap = new HashMap<Integer, Integer>();
         for (int i = 0; i < nums.length; i++) {
-            int diff = target-nums[i];
+            int diff = target - nums[i];
             if (indexMap.keySet().contains(diff)) {
                 return new int[] {i, indexMap.get(diff)};
             } else {
